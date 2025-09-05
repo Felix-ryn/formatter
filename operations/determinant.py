b29a8ba5a85290cdb93aeb3a38a9f219fbc165a8
Felix-ryn <felixagusta24@gmail.com>
2025-09-05 16:05:52 +0000

    refactor: Move determinant calculation to operations/determinant.py

---
+++ operations/determinant.py
@@ -0,0 +1,21 @@
+from ..utilities.validators import is_square
+
+def find_determinant(matrix):
+    """
+    Menghitung determinan dari matriks persegi 2x2 atau 3x3.
+    """
+    n = len(matrix)
+    if not is_square(matrix):
+        raise ValueError("Matrix must be a square matrix to find its determinant.")
+
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    elif n == 3:
+        det = (
+            matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1])
+            - matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0])
+            + matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0])
+        )
+        return det
+    else:
+        raise NotImplementedError("Determinant calculation is only supported for 2x2 and 3x3 matrices.")
